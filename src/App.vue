a6b3a850e3a2d7be889b9ebe230bceda9afd4fa9
叶锦湛 <jinzhan.ye@zhenai.com>
2022-06-24 18:52:54 +0800

    chore: 添加环境变量

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+const requestUrl = import.meta.env.VITE_REQUEST_URL
 </script>
 
 <template>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <div class="support-sass">support-sass</div>
+    <div>requestUrl:{{ requestUrl }}</div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -17,5 +21,8 @@ import HelloWorld from './components/HelloWorld.vue'
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+    .support-sass {
+        color: #42b983;
+    }
 }
 </style>
